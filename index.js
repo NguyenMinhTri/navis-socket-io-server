ea561c1762706b0accc7edabd3b62cef321de216
NguyenMinhTri <nmtri.uit@gmail.com>
2022-04-03 22:08:28 +0700

    update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const io = require('socket.io')(http, {
 	  origin: ['*']
   }
 });
-
+const { io : ioClient } = require("socket.io-client");
 const port = process.env.PORT || 3001;
 
 const cors = require('cors')({ origin: true });
@@ -19,10 +19,44 @@ app.use(function(req, res, next) {
 app.get('/socket-io', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+app.get('/api/testnoti2', (req, res) => {
+  console.log('api/testnoti');
+ 
+        const io2 = require('socket.io-client');
+        const socket2 = io2.connect('https://navis-socket-io-server.herokuapp.com');
+
+        socket2.on('connect', () => {
+          socket2.emit('navis', 'Assistant'); //login as "Assistant"
+          console.log('Successfully connected!');
+        });
+         //start - socket io
+         var socket = require('socket.io-client').connect('https://navis-socket-io-server.herokuapp.com');
+ 
+         socket.on('connect', function(){
+             console.log('connected');
+                         
+             socket.on('login', function(data){
+                 console.log('login');
+                 socket.emit('new message', 'StartEngine'); //sends message to chat server
+             });
+             
+             socket.on('got message', function() {
+                 socket.disconnect();
+             });
+             
+             socket.emit('add user', 'Assistant'); //login as "Assistant"
+         });
+         //end - socket io
+ 
+         //start - response to google assistant
+ 
+  
+         res.status(200).send("ok");
+});
 var bufferHeader = null;
 
 io.on('connection', (socket) => {
-	console.log("hello");
+	console.log("hello", socket.id);
   socket.on('chat message', msg => {
     io.emit('chat message', msg);
   });
